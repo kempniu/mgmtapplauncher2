3074f924c025e2c21363e4e672f2cc715242b71f
Michał Kępień <github@kempniu.pl>
2013-01-03 14:50:13 +0100

    Version bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Management App Launcher 2.1")]
+[assembly: AssemblyTitle("Management App Launcher 2.2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Michał Kępień")]
@@ -51,4 +51,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.2.*")]
